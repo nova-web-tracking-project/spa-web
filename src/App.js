6d2e129b496e4d8a8fd63ba4bda82ae2babebd13
menaka <menakalakshan92@gmail.com>
2025-08-28 13:16:52 +0800

    2

--- src/App.js
+++ src/App.js
@@ -5,9 +5,24 @@ import AddTransaction from './Components/AddTransaction';
 import { GlobalProvider } from './Context/GlobleState';
 import NewTransactionList from './Components/NewTransactionList';
 import NewIncomeExpenses from './Components/NewIncomeExpenses';
+import {useEffect} from "react";
 
 
 function App() {
+    useEffect(() => {
+        // Load GA script
+        const script = document.createElement("script");
+        script.src = "https://www.googletagmanager.com/gtag/js?id=G-6ZPERBXNPQ";
+        script.async = true;
+        document.head.appendChild(script);
+
+        // Init GA
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){window.dataLayer.push(arguments);}
+        gtag("js", new Date());
+        gtag("config", "G-6ZPERBXNPQ");
+    }, []);
+
   return (
    <GlobalProvider>
    <Header />
